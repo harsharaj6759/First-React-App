2c275f74db1bf572c7a8e9609acea4811e3922a4
harsharaj6759 <61135601+harsharaj6759@users.noreply.github.com>
2022-03-08 03:17:57 +0530

    lat, Long

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null, longitude: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      (error) => console.log(error)
+    );
+  }
+  render() {
+    return (
+      <div>
+        {" "}
+        My latitute is: {this.state.latitude} <br />
+        My Longitude is: {this.state.longitude}
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
